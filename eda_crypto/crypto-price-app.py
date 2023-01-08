2fe8bc0d01352120a1744263931fb7840ca28cb4
Will Torres, Jr <will.torres.jr@gmail.com>
2023-01-08 09:36:54 -0500

    Update crypto-price-app.py

--- eda_crypto/crypto-price-app.py
+++ eda_crypto/crypto-price-app.py
@@ -1,6 +1,6 @@
 # This app is for educational purpose only. Insights gained is not financial advice. Use at your own risk!
 import streamlit as st
-from PIL import Image
+from pillow import Image
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
