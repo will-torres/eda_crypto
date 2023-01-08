2fe64277948ea6a63f6ef6cea99f42537e50a150
Will Torres, Jr <will.torres.jr@gmail.com>
2023-01-07 23:51:19 -0500

    Update crypto-price-app.py

--- eda_crypto/crypto-price-app.py
+++ eda_crypto/crypto-price-app.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide")
 #---------------------------------#
 # Title
 
-image = Image.open('logo.jpg')
+image = Image.open('will-torres/eda_crypto/eda_crypto/logo.jpg')
 
 st.image(image, width = 500)
 
